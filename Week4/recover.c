27ce19d3e04cfbbd6d8d7e8f048b76cb2857bf45
hayden1126 <haydenleung1126@gmail.com>
2022-08-20 13:51:30 +0800

    completed week 4 recover.c

--- Week4/recover.c
+++ Week4/recover.c
@@ -5,8 +5,6 @@
 
 const int BLOCK_SIZE = 512;
 
-bool is_jpeg(uint8_t file[BLOCK_SIZE], int i);
-
 int main(int argc, char *argv[])
 {
     // Check command-line arguments
@@ -15,7 +13,6 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
-
     // Open file
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
@@ -23,44 +20,45 @@ int main(int argc, char *argv[])
         printf("Could not open file.\n");
         return 1;
     }
-
-    int next_jpeg;
-    uint8_t card[BLOCK_SIZE];
-    while (fread(card, sizeof(uint8_t), BLOCK_SIZE, file) == BLOCK_SIZE)
+    // Declare/assign some variables
+    int jpeg_count = 0;
+    bool jpeg_started = false;
+    FILE *img = NULL;
+    char *filename = malloc(sizeof(char) * 8);
+    uint8_t buffer[BLOCK_SIZE];
+    // While not end of the file
+    while (fread(buffer, sizeof(uint8_t), BLOCK_SIZE, file) == BLOCK_SIZE)
     {
-        // For each byte
-        for (int i = 0; i < BLOCK_SIZE - 4; i++)
+        // If current 512 byte block is the start of a JPEG file
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // If this byte is the start of a JPEG
-            if (!is_jpeg(card, i))
+            // If previous JPEG has been created, close that file
+            if (jpeg_started)
+            {
+                fclose(img);
+            }
+            jpeg_started = true;
+            // Name the JPEG file with the jpeg_count then add one to jpeg_count
+            sprintf(filename, "%03i.jpg", jpeg_count);
+            jpeg_count++;
+            // Create new JPEG and start writing to it
+            img = fopen(filename, "w");
+            fwrite(buffer, sizeof(uint8_t), BLOCK_SIZE, img);
+        }
+        // If current 512 byte block is NOT the start of a JPEG file
+        else
+        {
+            // If haven't found first JPEG, then go to next block of 512 bytes
+            if (!jpeg_started)
             {
                 continue;
             }
-            // Check location of the next JPEG
-            for (int j = i + 4; j < BLOCK_SIZE - 4; j++)
-            {
-                if (is_jpeg(card, j))
-                {
-                    next_jpeg = j;
-                    break;
-                }
-            }
-
+            // If JPEG already started, then write the info to the previous JPEG
+            fwrite(buffer, sizeof(uint8_t), BLOCK_SIZE, img);
         }
-
     }
+    // Free memory stuff
+    free(filename);
+    fclose(img);
     fclose(file);
-}
-
-bool is_jpeg(uint8_t file[BLOCK_SIZE], int i)
-{
-    if (file[i] != 0xff || file[i + 1] != 0xd8 || file[i + 2] != 0xff)
-    {
-        return false;
-    }
-    else if (file[i + 3] >= 0xe0 && file[i + 3] <= 0xef)
-    {
-        return true;
-    }
-    return false;
 }
